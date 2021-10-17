4f059b7b96e54a237edd8660bd6be1de2aee1757
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-10-17 11:29:52 -0300

    Ajustes no código para relazação do deploy

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -24,23 +24,23 @@ export default function PostPreview({ post }: PostPreviewProps) {
 
   useEffect(() => {
     if (session?.activeSubscription) {
-      router.push(`/posts/${post.slug}`)
+      router.push(`/posts/${post?.slug}`)
     }
   }, [session])
 
   return (
     <>
       <Head>
-        <title>{post.title} | Ignews</title>
+        <title>{post?.title} | Ignews</title>
       </Head>
 
       <main className={styles.container}>
         <article className={styles.post}>
-          <h1>{post.title}</h1>
-          <time>{post.updatedAt}</time>
+          <h1>{post?.title}</h1>
+          <time>{post?.updatedAt}</time>
           <div
             className={`${styles.postContent} ${styles.previewContent}`}
-            dangerouslySetInnerHTML={{ __html: post.content }}
+            dangerouslySetInnerHTML={{ __html: post?.content }}
           />
 
           <div className={styles.continueReading}>
@@ -71,8 +71,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   const post = {
     slug,
-    title: RichText.asText(response.data.title),
-    content: RichText.asHtml(response.data.content.splice(0, 3)),
+    title: RichText.asText(response.data?.title),
+    content: RichText.asHtml(response.data?.content.splice(0, 3)),
     updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
       day: '2-digit',
       month: 'long',
